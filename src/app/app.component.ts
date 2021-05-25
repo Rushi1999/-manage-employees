25a040baf242ab39db94bdc0fd56cae9c99c48d0
Rushikesh Jankar <rushijankar1999@gmail.com>
2021-05-25 23:25:47 +0530

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { UserloginComponent } from "./userlogin/userlogin.component";
+import { NewemployeeComponent } from "./newemployee/newemployee.component";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { UserloginComponent } from "./userlogin/userlogin.component";
+import { NewemployeeComponent } from "./newemployee/newemployee.component";
+
+const appRoutes: Routes = [
+  // { path: "", redirectTo: "userlogin", pathMatch: "full" },
+  {
+    path: "",
+    component: UserloginComponent,
+  },
+  {
+    path: "add-employee",
+    component: NewemployeeComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserloginComponent,
+    NewemployeeComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
